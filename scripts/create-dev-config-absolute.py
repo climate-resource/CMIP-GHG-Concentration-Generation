abfcb135818c442618e202f7de6fed105125cafc
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2023-11-04 15:06:25 +0100

    Add script to create absolute dev config

---
+++ scripts/create-dev-config-absolute.py
@@ -0,0 +1,30 @@
+"""
+Create dev config with absolute paths
+
+This should be run before working with the notebooks in dev mode to make sure
+that you don't end up with files everywhere.
+"""
+from __future__ import annotations
+
+from pathlib import Path
+
+from local.config import Config, converter_yaml
+from local.pydoit_nb.config_handling import insert_path_prefix
+
+DEV_FILE: Path = Path("dev-config.yaml")
+DEV_ABSOLUTE_FILE: Path = Path("dev-config-absolute.yaml")
+ROOT_OUTPUT_DIR: Path = Path(__file__).parent.parent.absolute()
+RUN_ID: str = "dev-run"
+
+with open(DEV_FILE) as fh:
+    config_relative = converter_yaml.loads(fh.read(), Config)
+
+config = insert_path_prefix(
+    config=config_relative,
+    prefix=ROOT_OUTPUT_DIR / RUN_ID,
+)
+
+with open(DEV_ABSOLUTE_FILE, "w") as fh:
+    fh.write(converter_yaml.dumps(config))
+
+print(f"Updated {DEV_ABSOLUTE_FILE}")
--- src/local/pydoit_nb/config_handling.py
+++ src/local/pydoit_nb/config_handling.py
@@ -4,9 +4,15 @@ Tools for working with configuration
 from __future__ import annotations
 
 from collections.abc import Callable, Iterable
+from pathlib import Path
 from typing import Any, TypeVar
 
+import attrs
+import numpy as np
+from attrs import evolve, fields
+
 T = TypeVar("T")
+U = TypeVar("U")
 
 
 def get_value_across_config_bundles(
@@ -49,3 +55,46 @@ def get_value_across_config_bundles(
         return vals[0]
 
     return vals
+
+
+def insert_path_prefix(config: T, prefix: Path) -> T:
+    """
+    Insert path prefix into config attributes
+
+    This adds the prefix ``prefix`` to any attributes of ``config`` which are
+    :obj:`Path`
+
+    Parameters
+    ----------
+    config
+        Config to update
+
+    prefix
+        Prefix to add to paths
+
+    Returns
+    -------
+        Updated ``config``
+    """
+
+    def update_attr_value(value: U) -> U:
+        if isinstance(value, Path):
+            return prefix / value
+
+        if attrs.has(value):
+            return insert_path_prefix(value, prefix)
+
+        if not isinstance(value, str | np.ndarray) and isinstance(value, Iterable):
+            return [update_attr_value(v) for v in value]
+
+        return value
+
+    config_attrs = fields(config.__class__)
+    evolutions: dict[str, Any] = {}
+    for attr in config_attrs:
+        attr_name = attr.name
+        attr_value = getattr(config, attr_name)
+
+        evolutions[attr_name] = update_attr_value(attr_value)
+
+    return evolve(config, **evolutions)
