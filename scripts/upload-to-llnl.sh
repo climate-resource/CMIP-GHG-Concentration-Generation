06a564f8820c82260d6671d274fa767b40a45fff
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-08-01 12:00:34 +0200

    Upload

--- scripts/upload-to-llnl.sh
+++ scripts/upload-to-llnl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUTPUT_BUNDLE_DIR="v0.3.0"
-for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-ready/input4MIPs/CMIP6Plus/CMIP/CR/CR-CMIP-0-2-0/atmos/yr/*; do
+for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-ready/input4MIPs/CMIP6Plus/CMIP/CR/CR-CMIP-0-3-0/atmos/yr/*; do
 	echo "Uploading ${variable_dir}"
 	pixi run input4mips-validation --logging-level INFO \
 		upload-ftp \
@@ -12,7 +12,7 @@ for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-re
 		"${variable_dir}" || exit 1
 done
 
-for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-ready/input4MIPs/CMIP6Plus/CMIP/CR/CR-CMIP-0-2-0/atmos/mon/*; do
+for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-ready/input4MIPs/CMIP6Plus/CMIP/CR/CR-CMIP-0-3-0/atmos/mon/*; do
 	echo "$variable_dir"
 	pixi run input4mips-validation --logging-level INFO \
 		upload-ftp \
