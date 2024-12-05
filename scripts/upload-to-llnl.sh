24c8f7abec654c27c9fefbcac72c7f4911a062ba
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-12-05 14:32:34 +0100

    Update input4mips validation version

--- scripts/upload-to-llnl.sh
+++ scripts/upload-to-llnl.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-OUTPUT_BUNDLE_DIR="v0.3.0"
-for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-ready/input4MIPs/CMIP6Plus/CMIP/CR/CR-CMIP-0-3-0/atmos/yr/*; do
+OUTPUT_BUNDLE_DIR="v0.4.0"
+OUTPUT_SOURCE_ID="CR-CMIP-0-4-0"
+UPLOAD_DIR="cr-cmip-0-4-0-1"
+for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-ready/input4MIPs/CMIP6Plus/CMIP/CR/"${OUTPUT_SOURCE_ID}"/atmos/yr/*; do
 	echo "Uploading ${variable_dir}"
 	pixi run input4mips-validation --logging-level INFO \
 		upload-ftp \
-		--ftp-dir-rel-to-root cr-cmip-0-3-0-2 \
+		--ftp-dir-rel-to-root "${UPLOAD_DIR}" \
 		--password zebedee.nicholls@climate-resource.com \
 		--n-threads 6 \
-		--cv-source "gh:cr-cmip-0-3-0" \
+		--cv-source "gh:main" \
 		"${variable_dir}" || exit 1
 done
 
-for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-ready/input4MIPs/CMIP6Plus/CMIP/CR/CR-CMIP-0-3-0/atmos/mon/*; do
-	echo "$variable_dir"
+for variable_dir in output-bundles/"${OUTPUT_BUNDLE_DIR}"/data/processed/esgf-ready/input4MIPs/CMIP6Plus/CMIP/CR/"${OUTPUT_SOURCE_ID}"/atmos/mon/*; do
+	echo "Uploading $variable_dir"
 	pixi run input4mips-validation --logging-level INFO \
 		upload-ftp \
-		--ftp-dir-rel-to-root cr-cmip-0-3-0-2 \
+		--ftp-dir-rel-to-root "${UPLOAD_DIR}" \
 		--password zebedee.nicholls@climate-resource.com \
 		--n-threads 6 \
-		--cv-source "gh:cr-cmip-0-3-0" \
+		--cv-source "gh:main" \
 		"${variable_dir}" || exit 1
 done
