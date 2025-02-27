f30629b32d7ade93da129249f025ba0201506fdd
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-02-27 18:52:00 +0100

    Add Trudinger steps into notebook steps

---
+++ src/local/notebook_steps/retrieve_and_process_trudinger_et_al_2016_data.py
@@ -0,0 +1,97 @@
+"""
+Retrieve and process Trudinger et al. (2016) data notebook steps
+"""
+
+from __future__ import annotations
+
+from collections.abc import Iterable
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from pydoit_nb.config_handling import get_config_for_step_id
+from pydoit_nb.notebook import ConfiguredNotebook, UnconfiguredNotebook
+from pydoit_nb.notebook_step import UnconfiguredNotebookBasedStep
+
+if TYPE_CHECKING:
+    from ..config.base import Config, ConfigBundle
+
+
+def configure_notebooks(
+    unconfigured_notebooks: Iterable[UnconfiguredNotebook],
+    config_bundle: ConfigBundle,
+    step_name: str,
+    step_config_id: str,
+) -> list[ConfiguredNotebook]:
+    """
+    Configure notebooks
+
+    Parameters
+    ----------
+    unconfigured_notebooks
+        Unconfigured notebooks
+
+    config_bundle
+        Configuration bundle from which to take configuration values
+
+    step_name
+        Name of the step
+
+    step_config_id
+        Step config ID to use when configuring the notebook
+
+    Returns
+    -------
+        Configured notebooks
+    """
+    uc_nbs_dict = {nb.notebook_path: nb for nb in unconfigured_notebooks}
+
+    config = config_bundle.config_hydrated
+
+    config_step = get_config_for_step_id(config=config, step=step_name, step_config_id=step_config_id)
+
+    configured_notebooks = [
+        ConfiguredNotebook(
+            unconfigured_notebook=uc_nbs_dict[
+                Path("015y_process-trudinger-et-al-2016-data") / "0151_download-trudinger-et-al-2016-data"
+            ],
+            configuration=(config_step.supplement,),
+            dependencies=(),
+            targets=(config_step.download_complete_file,),
+            config_file=config_bundle.config_hydrated_path,
+            step_config_id=step_config_id,
+        ),
+        ConfiguredNotebook(
+            unconfigured_notebook=uc_nbs_dict[
+                Path("015y_process-trudinger-et-al-2016-data") / "0152_process-trudinger-et-al-2016-data"
+            ],
+            configuration=(),
+            dependencies=(config_step.download_complete_file,),
+            targets=(config_step.processed_data_file,),
+            config_file=config_bundle.config_hydrated_path,
+            step_config_id=step_config_id,
+        ),
+    ]
+
+    return configured_notebooks
+
+
+step: UnconfiguredNotebookBasedStep[Config, ConfigBundle] = UnconfiguredNotebookBasedStep(
+    step_name="retrieve_and_process_trudinger_et_al_2016_data",
+    unconfigured_notebooks=[
+        UnconfiguredNotebook(
+            notebook_path=Path("015y_process-trudinger-et-al-2016-data")
+            / "0151_download-trudinger-et-al-2016-data",
+            raw_notebook_ext=".py",
+            summary="Retrieve and process Trudinger et al. (2016) data - download",
+            doc="Download Trudinger et al. (2016) data",
+        ),
+        UnconfiguredNotebook(
+            notebook_path=Path("015y_process-trudinger-et-al-2016-data")
+            / "0152_process-trudinger-et-al-2016-data",
+            raw_notebook_ext=".py",
+            summary="Retrieve and process Trudinger et al. (2016) data - process",
+            doc="Process Trudinger et al. (2016) data",
+        ),
+    ],
+    configure_notebooks=configure_notebooks,
+)
