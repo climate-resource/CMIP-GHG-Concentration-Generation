9f0091e2d19977c7eca9d3f6096200f19032169c
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-02-27 14:54:57 +0100

    Add new Menking data

--- src/local/config_creation/menking_et_al_2025_handling.py
+++ src/local/config_creation/menking_et_al_2025_handling.py
@@ -16,7 +16,7 @@ RETRIEVE_AND_PROCESS_MENKING_ET_AL_2025_DATA_STEPS = [
     RetrieveExtractMenkingEtal2025Data(
         step_config_id="only",
         raw_data_file=RAW_DIR / "spline-fits-for-ZN_CMIP7.xlsx",
-        expected_hash="bf832acdf24460e88312bda558200c60",
+        expected_hash="20df9337cddb739dc805e53847a19424",
         processed_data_file=Path("data/interim/menking-et-al-2025/menking_et_al_2025.csv"),
     )
 ]
